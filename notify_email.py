d4103dd52fe73058695829809fdcc3dc57e3ac2e
prthapa <prthapa@equinix.com>
2020-11-30 13:51:34 -0800

    notify email details from yaml

--- notify_email.py
+++ notify_email.py
@@ -1,8 +1,10 @@
 import smtplib
+import yaml
 
-SERVER = "mailhost.corp.equinix.com"
-FROM = "prthapa@equinix.com"
-TO = ["prthapa@equinix.com", "afsmohammad@equinix.com", "tadas@equinix.com"]  # must be a list
+email_details = yaml.safe_load(open("./email_info,yaml"))
+SERVER = email_details["server"]
+FROM = email_details["from"]
+TO = email_details["to"]  # must be a list
 
 SUBJECT = "Router Backup Failed"
 
@@ -13,6 +15,21 @@ def failed_notify(text):
     message = f"""From: {FROM}\r\nTo: {", ".join(TO)}\r\nSubject: {SUBJECT}\r\n\
 
     {text}
+    """
+
+    # Send the mail
+    server = smtplib.SMTP(SERVER)
+    server.sendmail(FROM, TO, message)
+    server.quit()
+
+
+def success_notify(log, testbed):
+    # Prepare actual message
+
+    message = f"""From: {FROM}\r\nTo: {", ".join(TO)}\r\nSubject: {SUBJECT}\r\n\
+    
+    Successfully completed router backup for {testbed}
+    {log}
     """
 
     # Send the mail
